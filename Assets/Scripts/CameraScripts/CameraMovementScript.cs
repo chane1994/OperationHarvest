9a0714954f8689f11b8bf457eb09731ba9dbe96d
Chane Grant <lunaharvest21@gmail.com>
2015-08-30 16:16:56 -0400

    I had started Character movement and have got the Droid girl setup and moving left and right. I still have some more stuff to take care of

---
+++ Assets/Scripts/CameraScripts/CameraMovementScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMovementScript : MonoBehaviour {
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = GameObject.FindGameObjectWithTag ("Player");
+		transform.position = new Vector3 (player.transform.position.x, player.transform.position.y +2 , player.transform.position.z-5);
+
+	}
+
+	void FixedUpdate()
+	{
+		Debug.Log(player.transform.position);
+		transform.position = new Vector3 (0, player.transform.position.y +2 , player.transform.position.z-5);
+
+	}
+	// Update is called once per frame
+	void Update () {
+
+
+	}
+}
---
+++ Assets/Scripts/CharacterScripts/CharacterMovementScript.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class CharacterMovementScript : MonoBehaviour {
+	public Animator charAnimator; //Access to Agent Controller, the animation controller of the model
+	public GameObject player;
+	public Camera camera;
+	public float movingSpeed;
+	bool _moving;
+	// Use this for initialization
+	void Start () {
+		player = this.gameObject;
+		camera = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		player.transform.eulerAngles = new Vector3 (0, 0, 0);
+		charAnimator = player.GetComponent<Animator> ();
+		charAnimator.SetBool ("moving", false); // Means that the character is not movin, genrally in the idle 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Handles's Basic movement of the Character
+		if (Input.GetButton ("Horizontal")) {
+
+			Debug.Log ("Moving");
+			Debug.Log (Input.GetAxis("Horizontal"));
+			_moving = true;
+			AnimationHandler ();
+
+			if (Input.GetAxis ("Horizontal") > 0) {
+				player.transform.eulerAngles = new Vector3 (0, 180, 0);
+			} else {
+				player.transform.eulerAngles = new Vector3 (0, 0, 0);
+			}
+			player.transform.Translate (Vector3.left * movingSpeed * -Input.GetAxis ("Horizontal"), camera.transform);
+				
+			
+			//player.transform.Translate(Vector3.left);
+		
+		} else {
+			_moving = false;
+			Debug.Log ("Not Moving");
+			AnimationHandler ();
+		}
+}
+/// <summary>
+	/// Handles all the change of parameters for the animator controller
+	/// </summary>
+	void AnimationHandler()
+	{
+		charAnimator.SetBool ("moving", _moving);
+	}
+}

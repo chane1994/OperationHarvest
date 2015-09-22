9015ad840d6313e690df1489d9d9477ae2680326
rewq455 <ryazap@gmail.com>
2015-09-22 12:15:53 -0400

    Added Sound Alarm
    See title

--- Assets/Scripts/EnemyScripts/EnemyCamera.cs
+++ Assets/Scripts/EnemyScripts/EnemyCamera.cs
@@ -7,8 +7,14 @@ public class EnemyCamera : MonoBehaviour {
 	public Light light3;
 	public Light light4;
 	public Light light5;
+    public AudioSource audio1;
+    public AudioSource audio2;
+    public AudioSource audio3;
+    public AudioSource audio4;
+    public AudioSource audio5;
 	public Light cameraLight;
 	public GameObject player;
+    public bool currentlyPlaying = false;
 	// Use this for initialization
 	void Start () 
 	{
@@ -18,7 +24,7 @@ public class EnemyCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
 	{
-		if (player.transform.position.y >= 6 && player.transform.position.x>-2.7 && player.transform.position.x<6.2) 
+		if (player.transform.position.y >= 6 && player.transform.position.x>-2.7 && player.transform.position.x<6.2 && !currentlyPlaying) 
 		{
 			cameraLight.color = Color.red;
 			light1.intensity = 8;
@@ -26,6 +32,12 @@ public class EnemyCamera : MonoBehaviour {
 			light3.intensity = 8;
 			light4.intensity = 8;
 			light5.intensity = 8;
+            audio1.Play();
+            audio2.Play();
+            audio3.Play();
+            audio4.Play();
+            audio5.Play();
+            currentlyPlaying = true;
 		}
 		
 	}
